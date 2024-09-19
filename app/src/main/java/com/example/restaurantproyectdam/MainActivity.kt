583977cb46679cb6f56d52604075200f22c6d356
RKCbas <sebashp2015@hotmail.com>
2024-09-19 11:09:03 -0600

    Created a text to each screen to represent in which screen you´re at and also added a lazy column with buttons to navigate between screens

--- app/src/main/java/com/example/restaurantproyectdam/MainActivity.kt
+++ app/src/main/java/com/example/restaurantproyectdam/MainActivity.kt
@@ -7,28 +7,45 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.restaurantproyectdam.ui.screens.AdminOrdersScreen
+import com.example.restaurantproyectdam.ui.screens.AdminProductsScreen
+import com.example.restaurantproyectdam.ui.screens.HomeScreen
+import com.example.restaurantproyectdam.ui.screens.LoginScreen
+import com.example.restaurantproyectdam.ui.screens.OrdersScreen
+import com.example.restaurantproyectdam.ui.screens.RegisterScreen
 import com.example.restaurantproyectdam.ui.theme.RestaurantProyectDAMTheme
+
 
 //This is development
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        //enableEdgeToEdge()
         setContent {
-            RestaurantProyectDAMTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            ComposeMultiScreenApp()
         }
+
+//        setContent {
+//            RestaurantProyectDAMTheme {
+//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+//                    Greeting(
+//                        name = "Android",
+//                        modifier = Modifier.padding(innerPadding)
+//                    )
+//                }
+//            }
+//        }
     }
 }
 
@@ -45,5 +62,26 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 fun GreetingPreview() {
     RestaurantProyectDAMTheme {
         Greeting("Android")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ComposeMultiScreenApp(){
+    val navController = rememberNavController()
+    Surface (color = Color.White) {
+        SetupNavGraph(navController = navController)
+    }
+}
+
+@Composable
+fun SetupNavGraph (navController: NavHostController){
+    NavHost(navController = navController, startDestination = "home"){
+        composable("home"){ HomeScreen(navController) }
+        composable("login"){ LoginScreen(navController) }
+        composable("register") { RegisterScreen(navController) }
+        composable("orders") { OrdersScreen(navController) }
+        composable("adminOrders") { AdminOrdersScreen(navController) }
+        composable("adminProducts") { AdminProductsScreen(navController) }
     }
 }
