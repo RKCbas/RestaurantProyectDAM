1f06aced12f6be662c2049088548493cf16e95e9
RKCbas <sebashp2015@hotmail.com>
2024-09-19 08:16:52 -0600

    Created Development Branch

--- app/src/main/java/com/example/restaurantproyectdam/MainActivity.kt
+++ app/src/main/java/com/example/restaurantproyectdam/MainActivity.kt
@@ -13,6 +13,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.restaurantproyectdam.ui.theme.RestaurantProyectDAMTheme
 
+//This is development
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
