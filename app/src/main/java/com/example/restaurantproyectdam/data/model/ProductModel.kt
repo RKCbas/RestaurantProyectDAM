8dfe29a67ad3b6e63db5b9ea4b87223ee5beeb53
RKCbas <sebashp2015@hotmail.com>
2024-10-10 15:33:07 -0600

    Created a product model and a order card file

--- app/src/main/java/com/example/restaurantproyectdam/data/model/ProductModel.kt
+++ app/src/main/java/com/example/restaurantproyectdam/data/model/ProductModel.kt
@@ -3,5 +3,6 @@ package com.example.restaurantproyectdam.data.model
 data class ProductModel(
     val id: Int,
     val name:String,
-    val cost:Double
+    val cost:Double,
+    val categoria:String
 )
