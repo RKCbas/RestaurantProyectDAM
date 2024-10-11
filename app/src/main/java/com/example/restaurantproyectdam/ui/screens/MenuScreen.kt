c58075ab17ee2a996a0ca9c8c97fd793f7859ee8
tony <starreach33@gmail.com>
2024-10-11 08:22:24 -0600

    Uniformed code to invoke a Scaffold component on every Screen needed

--- app/src/main/java/com/example/restaurantproyectdam/ui/screens/MenuScreen.kt
+++ app/src/main/java/com/example/restaurantproyectdam/ui/screens/MenuScreen.kt
@@ -1,12 +1,30 @@
 package com.example.restaurantproyectdam.ui.screens
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
+import com.example.restaurantproyectdam.ui.components.BottomBar
+import com.example.restaurantproyectdam.ui.components.SearchButton
 
 @Composable
 fun MenuScreen (navController: NavController) {
-    Column {
-
+    Scaffold (
+        //color = Color.White
+        bottomBar={ BottomBar(navController = navController) },
+        floatingActionButton = { SearchButton(onClick = {}) }
+    ) { innerPadding->
+        Column(
+            modifier = Modifier.padding(innerPadding)
+        ){
+            Content()
+        }
     }
+}
+
+@Composable
+private fun Content(){ //CÓDIGO DE YAHAIRA
+
 }
