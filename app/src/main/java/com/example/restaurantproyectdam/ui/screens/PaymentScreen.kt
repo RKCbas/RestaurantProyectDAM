1fca315237a588088eebc62628e66edf6701edf0
stonxzz <cuentamlgpro@gmail.com>
2024-10-12 11:09:54 -0600

    The route was created with a parameter for the payment screen where the product ID is sent

--- app/src/main/java/com/example/restaurantproyectdam/ui/screens/PaymentScreen.kt
+++ app/src/main/java/com/example/restaurantproyectdam/ui/screens/PaymentScreen.kt
@@ -1,2 +1,36 @@
 package com.example.restaurantproyectdam.ui.screens
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import com.example.restaurantproyectdam.ui.components.BottomBar
+import com.example.restaurantproyectdam.ui.components.Header
+import com.example.restaurantproyectdam.ui.components.SearchButton
+
+var idp: Int ?=null
+
+@Composable
+fun PaymentScreen (navController: NavController, id: Int) {
+    naveController = navController
+    idp = id
+    Scaffold (
+        //color = Color.White
+        bottomBar={ BottomBar(navController = navController) },
+        floatingActionButton = { SearchButton(onClick = {}) }
+    ) { innerPadding->
+        Column(
+            modifier = Modifier.padding(innerPadding)
+        )
+        {
+            PaymentContent()
+        }
+    }
+}
+
+@Composable
+fun PaymentContent(){
+    Header("Pago")
+}
