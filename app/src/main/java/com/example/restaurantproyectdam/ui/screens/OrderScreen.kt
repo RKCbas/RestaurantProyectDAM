a25d43aa87d6bff455381dde1f48d1b80afe42cf
RKCbas <sebashp2015@hotmail.com>
2024-10-13 18:42:46 -0600

    Created the view of an order when you click on it displays the view of the products contained in the order

--- app/src/main/java/com/example/restaurantproyectdam/ui/screens/OrderScreen.kt
+++ app/src/main/java/com/example/restaurantproyectdam/ui/screens/OrderScreen.kt
@@ -1,22 +1,46 @@
 package com.example.restaurantproyectdam.ui.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import com.example.restaurantproyectdam.R
 import com.example.restaurantproyectdam.data.model.OrderCardModel
 import com.example.restaurantproyectdam.data.model.OrderProduct
 import com.example.restaurantproyectdam.data.model.ProductModel
+import com.example.restaurantproyectdam.ui.theme.OnPrimary
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.HorizontalDivider
 
 @Composable
-fun OrderScreen(navController:NavController, id:Int){
+fun OrderScreen(navController: NavController, id: Int) {
     val orderProductsArray = arrayOf(
         OrderProduct(
             amount = 1,
@@ -49,14 +73,141 @@ fun OrderScreen(navController:NavController, id:Int){
         OrderCardModel(id = 2, status = false, orderProducts = orderProductsArray),
         OrderCardModel(id = 3, status = true, orderProducts = orderProductsArray)
     )
+    val specificOrder = orderCardsArray.find { it.id == id }
     Box(
         modifier = Modifier
             .fillMaxSize()
             .background(Color.LightGray)
-    ){
-        Row ()
-        {
+    ) {
+        Column {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Color(0xFFDDDEDD))
+                    .padding(10.dp, 10.dp, 15.dp, 5.dp),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                IconButton(onClick = { navController.popBackStack() }) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = "Back arrow",
+                        tint = Color.Black
+                    )
+                }
+                Column {
+                    Text(
+                        text = "Order",
+                        color = Color.Black,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 20.sp
+                    )
+                    Text(
+                        text = "id # $id",
+                        color = Color.Black,
+                        fontSize = 16.sp
+                    )
+                }
+                Box(
+                    modifier = Modifier.width(24.dp)
+                ) {
+                    Text(text = "")
+                }
+            }
 
+            LazyColumn(
+                modifier = Modifier.padding(10.dp)
+            ) {
+                if (specificOrder != null) {
+                    items(specificOrder.orderProducts) { item ->
+                        Card(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(5.dp)
+                        ) {
+                            Row(
+                                modifier = Modifier.fillMaxWidth(),
+                                horizontalArrangement = Arrangement.Center
+                            ) {
+                                Text(
+                                    text = item.orderProduct.title,
+                                    fontSize = 20.sp,
+                                    fontWeight = FontWeight.Bold,
+                                    modifier = Modifier
+                                        .padding(5.dp)
+                                )
+                            }
+                            HorizontalDivider(thickness = 5.dp)
+                            Row(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(top = 5.dp)
+                            ) {
+                                Column {
+                                    Image(
+                                        modifier = Modifier
+                                            .width(150.dp)
+                                            .height(175.dp)
+                                            .padding(start = 10.dp),
+                                        painter = item.orderProduct.image,
+                                        contentDescription = "product Image",
+                                    )
+
+                                }
+                                Column(
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(5.dp),
+                                    verticalArrangement = Arrangement.Center,
+                                    horizontalAlignment = Alignment.CenterHorizontally
+                                ) {
+                                    Text(
+                                        text = "Description",
+                                        fontSize = 16.sp,
+                                        fontWeight = FontWeight.Bold,
+                                    )
+                                    Text(
+                                        text = item.orderProduct.description,
+                                        fontSize = 12.sp,
+                                        fontWeight = FontWeight.Normal,
+                                        modifier = Modifier.padding(bottom = 5.dp)
+                                    )
+                                    Text(
+                                        text = "Quantity",
+                                        fontSize = 16.sp,
+                                        fontWeight = FontWeight.Bold,
+                                    )
+                                    Text(
+                                        text = item.amount.toString(),
+                                        fontSize = 12.sp,
+                                        fontWeight = FontWeight.Normal,
+                                        modifier = Modifier.padding(bottom = 5.dp)
+                                    )
+                                    Text(
+                                        text = "Cost",
+                                        fontSize = 16.sp,
+                                        fontWeight = FontWeight.Bold,
+                                    )
+                                    val totalCost = item.amount * item.orderProduct.cost
+                                    Text(
+                                        text = totalCost.toString(),
+                                        fontSize = 12.sp,
+                                        fontWeight = FontWeight.Normal,
+                                    )
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun OrderScreenPreview() {
+
+    OrderScreen(rememberNavController(), id = 1)
+
 }
