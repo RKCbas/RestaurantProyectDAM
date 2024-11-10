67579ea24c5ac2e8de211c5d5516d6df8ee9d1dd
ArmandoVallejo <juanarmandovh20@gmail.com>
2024-11-10 13:25:36 -0600

    Add: Added all the screens of profile, and fixed design elements

--- app/src/main/java/com/example/restaurantproyectdam/data/model/ProfileOptionsModel.kt
+++ app/src/main/java/com/example/restaurantproyectdam/data/model/ProfileOptionsModel.kt
@@ -5,5 +5,6 @@ import androidx.compose.ui.graphics.vector.ImageVector
 data class ProfileOptionsModel(
     val id:Int,
     val title:String,
-    val icon:ImageVector
+    val icon:ImageVector,
+    val route: String
 )
